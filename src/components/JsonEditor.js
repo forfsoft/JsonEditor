b52c619b20c5da99810026c00b24ababf9202140
임형민 <>
2021-09-21 12:13:55 +0900

    no message

--- src/components/JsonEditor.js
+++ src/components/JsonEditor.js
@@ -9,40 +9,73 @@ const jsonData = [
   {"data1":"a","data2":"b"},
   {"data1":"a1","data2":"b2"}
 ]
-
+https://github.com/jdorn/json-editor/blob/master/src/validator.js
 export default function JsonEditor() {
+
+  function formRoot(key, data) {
+    console.log(key, data)
+    return (      
+            <Accordion key={key}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography>{data.data1}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                  malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+    )
+  }
+
+  function formBuild(data) {
+    return (
+      <div>
+        { 
+          Object.keys(data).map((e, i) => {
+            return formRoot(i, data[e]);
+          })
+        }
+      </div>
+    )
+  }
+
   return (
     <div>
-      <Accordion>
-        <AccordionSummary 
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>{jsonData[1].data1}</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+      {formBuild(jsonData)}
+      {/* <div>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>test</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
           </Typography>
-        </AccordionDetails>
-      </Accordion>
+          </AccordionDetails>
+        </Accordion>
       <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography>Accordion 2</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
           </Typography>
-        </AccordionDetails>
-      </Accordion>
+          </AccordionDetails>
+        </Accordion>
+      </div> */}
     </div>
   );
 }
